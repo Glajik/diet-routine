61ee4630cafe7ad2a0d2ac35716b9f4f24191fa7
Valentine <valyuscha2004@gmail.com>
2020-08-15 13:01:20 +0300

    Make structure of project + add Redux + add React-Router

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {applyMiddleware, compose, createStore} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './redux/reducers/rootReducer'
+
+import App from './components/App'
+import './index.css'
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
---
+++ web-client/src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+// Here we will combine all our reducers
+
+import {combineReducers} from 'redux'
+// import {counterReducer} from './counterReducer'
+
+const rootReducer = combineReducers({
+  // counter: counterReducer
+})
+
+export default rootReducer
