61da4e927dfd3648d102a5b942f3a4a867655d08
Viacheslav Glazhevskyi <v.glazhevskyi@gmail.com>
2020-08-28 17:05:51 +0300

    Glajik/feature/firestore (#27)
    * Connect firebase to web-client
    
    * Counter component now looks more clear.
    Add 'recompose' lib
    
    * Use 'prop-types' in Counter component. Fix enhance() fn.
    
    * Update package-lock.json

--- web-client/src/features/counter/Counter.js
+++ web-client/src/features/counter/Counter.js
@@ -1,33 +1,48 @@
 import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
+import { withHandlers } from 'recompose'
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  selectCount,
-} from './counterSlice'
+  useFirestoreConnect,
+  isLoaded,
+  isEmpty,
+  withFirestore,
+} from 'react-redux-firebase'
+import { compose } from 'redux'
+import p from 'prop-types'
+import { path } from 'ramda'
 import styles from './Counter.module.css'
 
-export function Counter() {
-  const count = useSelector(selectCount)
-  const dispatch = useDispatch()
+const collection = 'counters'
+const document = 'first'
+const firestoreDocPath = `${collection}/${document}`
+
+const Loading = () => <div>Loading...</div>
+const Empty = () => <div>No data</div>
+
+function Counter({ value, increment, decrement, loading, empty }) {
+  useFirestoreConnect([collection])
+
   const [incrementAmount, setIncrementAmount] = useState('2')
+
+  if (loading) return <Loading />
+  if (empty) return <Empty />
 
   return (
     <div>
       <div className={styles.row}>
         <button
+          type="button"
           className={styles.button}
           aria-label="Increment value"
-          onClick={() => dispatch(increment())}>
+          onClick={() => increment()}>
           +
         </button>
-        <span className={styles.value}>{count}</span>
+        <span className={styles.value}>{value}</span>
         <button
+          type="button"
           className={styles.button}
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}>
+          onClick={() => decrement()}>
           -
         </button>
       </div>
@@ -39,16 +54,59 @@ export function Counter() {
           onChange={e => setIncrementAmount(e.target.value)}
         />
         <button
+          type="button"
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}>
+          onClick={() => increment(Number(incrementAmount) || 0)}>
           Add Amount
         </button>
         <button
+          type="button"
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}>
+          onClick={() => increment(Number(incrementAmount) || 0)}>
           Add Async
         </button>
       </div>
     </div>
   )
 }
+
+Counter.propTypes = {
+  value: p.number,
+  increment: p.func.isRequired,
+  decrement: p.func.isRequired,
+  loading: p.bool,
+  empty: p.bool,
+}
+
+Counter.defaultProps = {
+  value: 0,
+  loading: false,
+  empty: true,
+}
+
+const firestoreDataSelector = state => state.firestore.data
+
+const mapStateToProps = state => {
+  const data = firestoreDataSelector(state)
+  const selectValue = path([collection, document, 'value'])
+  return {
+    loading: !isLoaded(data),
+    empty: isEmpty(data),
+    value: selectValue(data),
+  }
+}
+
+const updateCounterValue = ({ firestore, value }, delta) =>
+  firestore.update(firestoreDocPath, { value: value + delta })
+
+// Order is important
+const enhance = compose(
+  withFirestore,
+  connect(mapStateToProps),
+  withHandlers({
+    increment: props => (delta = 1) => updateCounterValue(props, delta),
+    decrement: props => (delta = -1) => updateCounterValue(props, delta),
+  })
+)
+
+export default enhance(Counter)
