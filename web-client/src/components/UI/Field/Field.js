d639c78a75888d1bdd0385b9666e271a7adc623f
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-10-16 22:56:19 +0300

    Use Valentine's Field component in Welcome Page

--- web-client/src/components/UI/Field/Field.js
+++ web-client/src/components/UI/Field/Field.js
@@ -6,56 +6,18 @@ import {
   Error
 } from './style'
 
-const field = (props) => {
-  let fieldType
-
-  switch (props.folderType) {
-    case 'input':
-      fieldType = (
-        <Input
-          style={props.style}
-          type={props.type}
-          value={props.value}
-          name={props.name}
-          placeholder={props.placeholder}
-          onChange={props.changed}/>
-      )
-      break
-    case 'textarea':
-      fieldType = (
-        <textarea
-          cols="30"
-          rows="10"
-          value={props.value}
-          name={props.name}
-          placeholder={props.placeholder}/>
-      )
-      break
-    case 'select':
-      fieldType = (
-        <select name={props.name}>
-          {props.options.map(option => <option>{option}</option>)}
-        </select>
-      )
-      break
-    default:
-      fieldType = (
-        <Input
-          type={props.type}
-          value={props.value}
-          name={props.name}
-          placeholder={props.placeholder}
-          onChange={props.changed}/>
-      )
-  }
-
-  return (
-    <FieldWrapper>
-      <Label>{props.label}</Label>
-      {fieldType}
-      <Error>{props.errorMessage}</Error>
-    </FieldWrapper>
-  )
-}
+const field = (props) => (
+  <FieldWrapper>
+    <Label>{props.label}</Label>
+    <Input
+        style={props.style}
+        type={props.type}
+        value={props.value}
+        name={props.name}
+        placeholder={props.placeholder}
+        onChange={props.onChange}/>
+    <Error>{props.errorMessage}</Error>
+  </FieldWrapper>
+)
 
 export default field
