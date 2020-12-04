99dd31c75b3d7e5d5a78a8f7922a7bb3ca31c750
Andriy Stolitniy <a.s.stoletniy@gmail.com>
2020-12-04 12:04:00 +0300

    changed route backbutton productsearch to mainpage

--- web-client/src/components/ProductSearch/ProductSearch.js
+++ web-client/src/components/ProductSearch/ProductSearch.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import {Container} from '../UI'
-import {TopBarLayout} from '../../layouts'
-import {ProductSearchContentWrapper} from './style'
+import { Container } from '../UI'
+import { TopBottomBarsLayout } from '../../layouts'
+import { ProductSearchContentWrapper } from './style'
 
 const ProductSearch = (props) => {
   return (
     <Container>
-      <TopBarLayout
+      <TopBottomBarsLayout
         title="productSearch"
+        settingsAction={() => console.log('Main Page')}
         history={props.history}>
         <ProductSearchContentWrapper>
           <h2>Product Search</h2>
-          <p style={{marginTop: 40}}>Now you can press comeback button. Then you can see that you will be returned to the last page, instead of the Main page.</p>
+          <p style={{ marginTop: 40 }}>Now you can press comeback button. Then you can see that you will be returned to the last page, instead of the Main page.</p>
         </ProductSearchContentWrapper>
-      </TopBarLayout>
+      </TopBottomBarsLayout>
     </Container>
   )
 }
