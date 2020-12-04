2429c0963bf8f0e73bb9bafef1672f5e8a02235c
Valentine <valyuscha2004@gmail.com>
2020-12-05 00:26:07 +0200

    Create basic structure of the calendar

--- web-client/src/components/Calendar/style.js
+++ web-client/src/components/Calendar/style.js
@@ -1,5 +1,75 @@
 import styled from 'styled-components'
 
 export const CalendarContentWrapper = styled.div`
-  margin: 65px 0;
+  margin: 65px 0 0;
+  
+`
+
+export const CalendarWrapper = styled.div`
+  
+`
+
+export const Table = styled.table`
+  box-sizing: border-box;
+  width: 100%;
+`
+
+export const Thead = styled.thead``
+
+export const CalendarHeader = styled.tr``
+
+export const DaysList = styled.tr`
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+  font-size: 12px;
+`
+
+export const Tbody = styled.tbody``
+
+export const WeekDaysTd = styled.td`
+  display: inline-block;
+  width: 14.285%;
+`
+
+export const WeekDaysSpan = styled.span`
+  display: flex;
+  justify-content: center;
+  width: 40px;
+  margin: 0 auto;
+`
+
+export const LastAndNextMonthsDatesTd = styled.td`
+  display: inline-block;
+  width: 14.285%;
+`
+
+export const LastAndNextMonthsDatesSpan = styled.span`
+  display: flex;
+  width: 40px;
+  height: 40px;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+`
+
+export const DateTd = styled.td`
+  display: inline-block;
+  width: 14.285%;
+`
+
+export const DateNum = styled.span`
+  display: flex;
+  width: 40px;
+  height: 40px;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+`
+
+export const DaysRow = styled.tr`
+  display: inline-block;
+  width: 100%;
 `
