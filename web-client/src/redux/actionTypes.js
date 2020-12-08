356ce674889a6d643811b1532a9d9a35ffcebb85
Vadim <vadimoleynik039@gmail.com>
2020-12-08 12:37:42 +0200

    add authreducer

--- web-client/src/redux/actionTypes.js
+++ web-client/src/redux/actionTypes.js
@@ -1,5 +1,13 @@
 // Here will be types of our actions
 
+export const INIT_SIGN_UP_FORM = 'INIT_SIGN_UP_FORM'
+export const INIT_SIGN_IN_FORM = 'INIT_SIGN_IN_FORM'
+
+export const FIELD_CHANGED = 'FIELD_CHANGED'
+export const CLEAR_FORM = 'CLEAR_FORM'
+export const DISABLED = 'DISABLED'
+export const NOT_DISABLED = 'NOT_DISABLED'
+
 export const LOAD = 'LOAD'
 export const NOT_LOAD = 'NOT_LOAD'
 
