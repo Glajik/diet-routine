31d1491f61698a8b6b25635523a56d7bf7a2d3d2
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-12-10 13:38:52 +0200

    Add updating current view tab feature

--- firestore-admin-tool/src/handlers/onOpen.js
+++ firestore-admin-tool/src/handlers/onOpen.js
@@ -3,6 +3,7 @@
 import { updateWithEntries } from '../services/spreadsheetService'
 import { showCredentialsDialog } from '../ui/credentialService' // eslint-disable-line
 import { getEntriesFromCollection } from '../services/firestoreService'
+import { toast } from '../utils/ui'
 
 // eslint-disable-next-line no-unused-vars
 function onOpen() {
@@ -10,8 +11,9 @@ function onOpen() {
   ui.createMenu('Admin Tool')
     .addSubMenu(
       ui.createMenu('Products')
-        .addItem('Get All', 'getDocsAndUpdateTab'),
+        .addItem('Get All', 'getProductsAndUpdateTab'),
     )
+    .addItem('Update tab', 'updateCurrentTab')
     .addSubMenu(
       ui.createMenu('Settings')
         .addItem('Set credentials', 'showCredentialsDialog'),
@@ -19,14 +21,37 @@ function onOpen() {
     .addToUi()
 }
 
+/**
+ * Update tab with data from the collection with same name.
+ */
 // eslint-disable-next-line no-unused-vars
-function getDocsAndUpdateTab() {
-  const allowedToUpdate = ['Products']
+function getDocsAndUpdateTab(name) {
+  const entries = getEntriesFromCollection(name)
+  updateWithEntries(name, entries)
+}
+
+/**
+ * Get products from `Products` collection and update tab
+ */
+// eslint-disable-next-line no-unused-vars
+function getProductsAndUpdateTab() {
+  getDocsAndUpdateTab('Products')
+}
+
+/**
+ * Update current "view" tab with data from the
+ * corresponding collection. Allowed tabs set in `allowedTabs`.
+ * Abort if not allowed.
+ */
+// eslint-disable-next-line no-unused-vars
+function updateCurrentTab() {
+  const allowedTabs = ['Products']
   const ss = SpreadsheetApp.getActiveSpreadsheet()
   const sheet = ss.getActiveSheet()
   const name = sheet.getName()
-  if (allowedToUpdate.includes(name)) {
-    const entries = getEntriesFromCollection(name)
-    updateWithEntries(name, entries)
+  if (allowedTabs.includes(name)) {
+    getDocsAndUpdateTab(name)
+    return
   }
+  toast('Abort. You can\'t update this tab from Firestore')
 }
