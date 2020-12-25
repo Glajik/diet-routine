f6e8120bd5f547912aeb7279f3b74d88b0488020
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-12-25 21:41:24 +0200

    Add menu to ProfilePage

--- web-client/src/components/ProfilePage/index.js
+++ web-client/src/components/ProfilePage/index.js
@@ -3,7 +3,16 @@ import { Container, BottomBar } from '../UI'
 import PageTitle from './PageTitle/index'
 import UserPhoto from './UserPhoto'
 import UserName from './UserName/index'
+import { List } from 'antd-mobile'
 import styles from './index.module.css'
+
+import {
+  CalculatorOutlined,
+  StarOutlined,
+  MailOutlined,
+  ReadOutlined,
+  LogoutOutlined
+} from '@ant-design/icons'
 
 
 const ProfilePage = ({ history }) => {
@@ -14,6 +23,44 @@ const ProfilePage = ({ history }) => {
       <PageTitle>Profile</PageTitle>
       <UserPhoto photoUrl={null}/>
       <UserName>John Doe</UserName>
+      <List className={styles.container}>
+        <List.Item
+          arrow="horizontal"
+          thumb={<CalculatorOutlined className={styles.icon}/>}
+          platform="android"
+          onClick={() => {}}
+        >
+          Calculate your daily calories
+        </List.Item>
+        <List.Item
+          arrow="horizontal"
+          thumb={<StarOutlined className={styles.icon}/>}
+          onClick={() => {}}
+        >
+          Favorites
+        </List.Item>
+        <List.Item
+          arrow="horizontal"
+          thumb={<MailOutlined className={styles.icon}/>}
+          onClick={() => {}}
+        >
+          Contact us
+        </List.Item>
+        <List.Item
+          arrow="horizontal"
+          thumb={<ReadOutlined className={styles.icon}/>}
+          onClick={() => {}}
+        >
+          Terms of use
+        </List.Item>
+        <List.Item
+          arrow="horizontal"
+          thumb={<LogoutOutlined className={styles.icon}/>}
+          onClick={() => {}}
+        >
+          Sign Out
+        </List.Item>
+      </List>
       <BottomBar history={history} />
     </Container>
   )
