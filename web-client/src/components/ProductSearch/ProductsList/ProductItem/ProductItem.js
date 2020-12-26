b5813640305a722ba718ac3bac2ddeab93023819
Vadim <vadimoleynik039@gmail.com>
2020-12-26 13:12:39 +0200

    add product list

--- web-client/src/components/ProductSearch/ProductsList/ProductItem/ProductItem.js
+++ web-client/src/components/ProductSearch/ProductsList/ProductItem/ProductItem.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 import styled from './ProductItem.module.css'
 import image from '../../../../assets/images/star.svg'
-const ProductItem = () => {
-    return (
-        <div className={styled.container}>
-            <img src={image} className={styled.star}/>
-            <div className={styled.product}>
-                <p className={styled.productName}>Abalone, raw</p>
-                <p className={styled.productDescriptions}>100 kcal / 100 g</p>
-            </div>  
-        </div>
-    )
+import star from '../../../../assets/images/star_active.svg'
+import plus from '../../../../assets/images/plus.svg'
+
+const ProductItem = ({product}) => {
+  const [isActive, setIsActive] = useState(false)
+  return (
+    <div className={styled.container}>
+      {isActive ? (
+        <img
+          src={star}
+          className={styled.star}
+          onClick={() => setIsActive(!isActive)}
+        />
+      ) : (
+        <img
+          src={image}
+          className={styled.star}
+          onClick={() => setIsActive(!isActive)}
+        />
+      )}
+      
+      <div className={styled.product}>
+        <p className={styled.productName}>{product.name}</p>
+        <p className={styled.productDescriptions}>{product.calories} kcal / 100 g</p>
+      </div>
+      <img src={plus} className={styled.plus} />
+    </div>
+  )
+}
+
+ProductItem.propTypes = {
+    product: PropTypes.object.isRequired
 }
 
 export default ProductItem
--- web-client/src/components/ProductSearch/ProductsList/ProductsList.js
+++ web-client/src/components/ProductSearch/ProductsList/ProductsList.js
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { Fragment, useMemo } from 'react'
+import { useFirestoreConnect } from 'react-redux-firebase'
+import { useSelector } from 'react-redux'
 
 import styled from './ProductsList.module.css'
 import ProductItem from './ProductItem/ProductItem.js'
 
+
 const ProductsList = () => {
-    return (
-        <div className={styled.container}>
-            <ProductItem />
-        </div>
-    )
+  useFirestoreConnect({
+    collection: 'Products',
+    storeAs: 'products',
+  })
+
+  const products = useSelector(state => state.firestore.data.products) || {}
+  console.log(Object.values(products))
+
+  const objWithCategories = useMemo(() => {
+    return Object.values(products)
+      .sort((a, b) => (a.name > b.name ? 1 : -1))
+      .reduce((acc, name) => {
+        let firstLetter = name.name[0].toLocaleUpperCase()
+        if (!acc[firstLetter]) {
+          acc[firstLetter] = { title: firstLetter, data: [name] }
+        } else {
+          acc[firstLetter].data.push(name)
+        }
+        return acc
+      }, {})
+  }, [products])
+
+  return (
+    <div className={styled.container}>
+      {Object.values(objWithCategories).map((item, i) => (
+        <Fragment key={i}>
+          <p className={styled.firstLetter}>- {item.title} -</p>
+          {item.data.map((product, index) => (
+            <ProductItem product={product} key={index} />
+          ))}
+        </Fragment>
+      ))}
+    </div>
+  )
 }
 
 export default ProductsList
