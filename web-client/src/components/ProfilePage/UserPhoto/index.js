f7e3be7ea67f935630921ee949c3a8124c1f4419
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-12-26 10:25:36 +0200

    Move IconButton to ProfilePage root, refactored UserPhoto

--- web-client/src/components/ProfilePage/UserPhoto/index.js
+++ web-client/src/components/ProfilePage/UserPhoto/index.js
@@ -1,3 +1,15 @@
-import UserPhoto from './UserPhoto'
+import React from 'react'
+import styles from './index.module.css'
+import UploadPhoto from './UploadPhoto'
+
+const UserPhoto = ({ photoURL, onUpload }) => (
+  <div className={styles.container}>
+    {
+      !photoURL
+        ? <UploadPhoto onUpload={onUpload} />
+        : <img src={photoURL} className={styles.image}/>
+    }
+  </div>
+)
 
 export default UserPhoto
