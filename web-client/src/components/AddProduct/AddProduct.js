38c24487e03fe40444bcceaa01ec64c060dac390
Valentine <valyuscha2004@gmail.com>
2020-12-28 20:11:00 +0200

    Make structure of add-product page

--- web-client/src/components/AddProduct/AddProduct.js
+++ web-client/src/components/AddProduct/AddProduct.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container} from '../UI'
 import {TopBarLayout} from '../../layouts'
-import {AddProductContentWrapper, Form, Input} from './style'
+import {categories} from './productCategories'
+import {
+  AddProductContentWrapper,
+  Form,
+  Input,
+  Select,
+  Option,
+  FieldWrapper,
+  CpfcLabel,
+  AmountUnitLabel,
+  SaveProductButton
+} from './style'
 
 const AddProduct = (props) => {
+  const[selectValue, setSelectValue] = useState('select_an_option')
+
+  const createProduct = (event) => {
+    event.preventDefault()
+    console.log('clicked')
+  }
+
   return (
     <Container>
       <TopBarLayout
@@ -11,7 +29,41 @@ const AddProduct = (props) => {
         history={props.history}>
         <AddProductContentWrapper>
            <Form>
-             <Input placeholder="Write"/>
+             <Input type="text" placeholder="Write a product name"/>
+             <Select value={selectValue} onChange={(e) => setSelectValue(e.target.value)}>
+               <Option
+                 value="select_an_option"
+                 style={{color: '#bfbfbf', display: 'none'}}
+                 disabled>Select a category</Option>
+               {categories.map((item, index) => (
+                 <Option value={item.id} key={index}>
+                   {item.name}
+                 </Option>
+               ))}
+             </Select>
+             <FieldWrapper>
+               <CpfcLabel>Proteins</CpfcLabel>
+               <Input type="number" placeholder="0" proteins/>
+               <AmountUnitLabel>g</AmountUnitLabel>
+             </FieldWrapper>
+             <FieldWrapper>
+               <CpfcLabel>Fats</CpfcLabel>
+               <Input type="number" placeholder="0" fats/>
+               <AmountUnitLabel>g</AmountUnitLabel>
+             </FieldWrapper>
+             <FieldWrapper>
+               <CpfcLabel>Carbs</CpfcLabel>
+               <Input type="number" placeholder="0" carbs/>
+               <AmountUnitLabel>g</AmountUnitLabel>
+             </FieldWrapper>
+             <FieldWrapper>
+               <CpfcLabel>Calories</CpfcLabel>
+               <Input type="number" placeholder="0" calories/>
+               <AmountUnitLabel>k</AmountUnitLabel>
+             </FieldWrapper>
+             <SaveProductButton onClick={createProduct}>
+               Save Product
+             </SaveProductButton>
            </Form>
         </AddProductContentWrapper>
       </TopBarLayout>
--- web-client/src/components/AddProduct/style.js
+++ web-client/src/components/AddProduct/style.js
@@ -6,15 +6,85 @@ export const AddProductContentWrapper = styled.div`
 
 export const Form = styled.form``
 
+export const FieldWrapper = styled.div`
+  position: relative;
+`
+
 export const Input = styled.input`
   width: 100%;
   border-width: 0 0 1px 0;
   border-style: solid;
   border-color: #bfbfbf;
   font-size: 16px;
-  font-weight: 600;
+  color: #262626;
+  margin-bottom: 25px;
+  padding: 10px 0;
   
   &::placeholder {
    color: #bfbfbf;
+   font-weight: 600;
   }
+    
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0; 
+  } 
+  
+  ${p => p.proteins ? `
+      text-align: right;
+      padding: 10px 25px 10px 65px;
+    ` : p.fats ? `
+        text-align: right;
+        padding: 10px 25px 10px 37px;
+    ` : p.carbs ? `
+        text-align: right;
+        padding: 10px 25px 10px 48px;
+    ` : p.calories ? `
+        text-align: right;
+        padding: 10px 25px 10px 65px;
+    ` : ''}
+`
+
+export const Select = styled.select`
+  width: 100%;
+  border-width: 0 0 1px 0;
+  border-style: solid;
+  border-color: #bfbfbf;
+  font-size: 16px;
+  font-weight: ${p => p.value !== 'select_an_option' ? 'normal' : 600};
+  font-family: 'Nunito', sans-serif;
+  padding: 10px 0;
+  color: ${p => p.value !== 'select_an_option' ? '#262626' : '#bfbfbf'};
+  margin-bottom: 25px;
+`
+
+export const Option = styled.option`
+  color: #262626;
+`
+
+export const CpfcLabel = styled.span`
+  position: absolute;
+  top: 7px;
+  font-size: 16px;
+  color: #262626;
+`
+
+export const AmountUnitLabel = styled.span`
+  position: absolute;
+  top: 7px;
+  font-size: 16px;
+  right: 0;
+  color: #595959;
+`
+
+export const SaveProductButton = styled.button`
+  margin: 43px 0 32px;
+  text-align: center;
+  padding: 12px 0;
+  width: 288px;
+  background: #3DAD06;
+  border: none;
+  border-radius: 30px;
+  color: #ffffff;
 `
